d31e8f4f5a986b3c55506a73e94e4a525884fef2
Evelien de Greef <49288304+edegreef@users.noreply.github.com>
2022-10-04 14:31:50 -0500

    Update 0-rename_scaffold_fasta.sh

--- sequence_data_prep/0-rename_scaffold_fasta.sh
+++ sequence_data_prep/0-rename_scaffold_fasta.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# want to make scaffold name match gff file
-# basically go from “ENA|SIHG01000001|SIHG01000001.1 Monodon monoceros isolate NGI Contig0_ctg1, whole genome shotgun seq” to just “SIHG01000001.1”
+# Quick step to make scaffold name match gff file (just for the narwhal genome)
+# Basically I want to go from “ENA|SIHG01000001|SIHG01000001.1 Monodon monoceros isolate NGI Contig0_ctg1, whole genome shotgun seq” to just “SIHG01000001.1”
 
-# keep characters 18-31
-# want to go by letter but not sure how, workaround is keep characters between the two "|" and then add ".1" to each one to match gff file
-
+# Want to keep characters 18-31, workaround is keep characters between the two "|" and then add ".1" to each one to match gff file
 awk -F '|' '/^>/ { $0 = ">" $2 } { print }' NAR_GCF_005190385.2.fasta | sed '/^>/s/$/.1/' > NAR_GCF_005190385.2.scafname.fasta
