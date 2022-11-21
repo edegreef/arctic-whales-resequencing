e107b4deacf7017f7799514de5529cbb8c37305f
Evelien de Greef <49288304+edegreef@users.noreply.github.com>
2022-11-21 10:00:42 -0600

    Update 04-plotsmc.sh

--- demography/smcpp/04-plotsmc.sh
+++ demography/smcpp/04-plotsmc.sh
@@ -2,4 +2,4 @@
 
 source /home/edegreef/smcpp/bin/activate
 
-smc++ plot -c smc_1group_plot.pdf smc_out_timepoints/group1/run_*/model.final.json
+smc++ plot -c smc_1group_plot.pdf smc_out_timepoints/1group/run_*/model.final.json
