ff2bca8bd3d72da122f8530117f3b94d2b93f18d
Evelien de Greef <49288304+edegreef@users.noreply.github.com>
2023-09-20 16:40:38 -0500

    Update 04-psmc_plot.sh

--- demography/psmc/04-psmc_plot.sh
+++ demography/psmc/04-psmc_plot.sh
@@ -1,5 +1,11 @@
 # bowhead whale
 /home/degreefe/programs/psmc/utils/psmc_plot.pl -R -u 2.68e-08 -g 52.3 -p 88_Pang.N25.t15.r5_52.3_plot 88_Pang.N25.t15.r5.psmc
 
+# bowhead whale bootstrap
+/home/degreefe/programs/psmc/utils/psmc_plot.pl -R -u 2.68e-08 -g 52.3 -pY50000 combined_mugen combined88_Pang.psmc
+
 # narwhal
 /home/degreefe/programs/psmc/utils/psmc_plot.pl -R -u 1.56e-08 -g 21.9 -p ARRB_99_1026_1.56e-8_g21.9plot ARRB_99_1026.N25.t15.r5.psmc
+
+# narwhal bootstrap
+/home/degreefe/programs/psmc/utils/psmc_plot.pl -R -pY50000 combined combined.ARRB_99_1026.psmc
