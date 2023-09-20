39227033e560ed494c44c6f0bf711f73c0ece95d
Evelien de Greef <49288304+edegreef@users.noreply.github.com>
2023-09-20 16:45:21 -0500

    Update 02-psmc_run.sh

--- demography/psmc/02-psmc_run.sh
+++ demography/psmc/02-psmc_run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# bowhead
 sample=88_Pang
+
+# narwhal
+#sample=ARRB_99_1026
 
 /home/degreefe/programs/psmc/psmc -N25 -t15 -r5 -p "4+25*2+4+6" -o $sample.N25.t15.r5.psmc $sample.psmcfa
