0ffe13ba9bc4780a400a92ef3e6fab0c77e33051
Evelien de Greef <49288304+edegreef@users.noreply.github.com>
2023-09-20 16:38:46 -0500

    Update and rename reminder to 04-psmc_plot.sh

---
+++ demography/psmc/04-psmc_plot.sh
@@ -0,0 +1,5 @@
+# bowhead whale
+/home/degreefe/programs/psmc/utils/psmc_plot.pl -R -u 2.68e-08 -g 52.3 -p 88_Pang.N25.t15.r5_52.3_plot 88_Pang.N25.t15.r5.psmc
+
+# narwhal
+/home/degreefe/programs/psmc/utils/psmc_plot.pl -R -u 1.56e-08 -g 21.9 -p ARRB_99_1026_1.56e-8_g21.9plot ARRB_99_1026.N25.t15.r5.psmc
