465220734d1e5e82a7316194365cd9945220ee83
Evelien de Greef <49288304+edegreef@users.noreply.github.com>
2023-10-03 12:15:28 -0500

    Update 05cc-map_fastq.sh

--- sequence_data_prep/05cc-map_fastq.sh
+++ sequence_data_prep/05cc-map_fastq.sh
@@ -5,8 +5,8 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=edegreef@ucdavis.edu
 #SBATCH --constraint=skylake
-#SBATCH --cpus-per-task=48
-#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=12
+#SBATCH --ntasks=4
 #SBATCH --nodes=1
 #SBATCH --mem=0
 #SBATCH --job-name=map_fastq_part2
@@ -19,7 +19,7 @@ module load StdEnv/2020 bwa/0.7.17 samtools/1.12
 cd /scratch/edegreef/whales/BOW_fastq/merged/trimmed
 
 # Map paired.fastq's to reference. 
-ls *_R1_paired.fastq.gz | sed 's/_R1_paired.fastq.gz$//' | parallel --jobs $SLURM_CPUS_PER_TASK 'bwa mem /scratch/edegreef/whales/ref_genomes/BOW_reference.fasta {}_R1_paired.fastq.gz {}_R2_paired.fastq.gz | samtools sort -T {}_tmp -o {}.sorted.bam'
+ls *_R1_paired.fastq.gz | sed 's/_R1_paired.fastq.gz$//' | parallel --jobs $SLURM_NTASKS 'bwa mem /scratch/edegreef/whales/ref_genomes/BOW_reference.fasta {}_R1_paired.fastq.gz {}_R2_paired.fastq.gz | samtools sort -T {}_tmp -o {}.sorted.bam'
 
 # Index bam file
 ls *.sorted.bam | parallel 'samtools index {}'
