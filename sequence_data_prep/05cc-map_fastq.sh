b711d900214a55be11ee41903a2f76d67b02e15f
Evelien de Greef <49288304+edegreef@users.noreply.github.com>
2024-06-12 12:27:10 -0500

    Update 05cc-map_fastq.sh

--- sequence_data_prep/05cc-map_fastq.sh
+++ sequence_data_prep/05cc-map_fastq.sh
@@ -18,8 +18,12 @@ module load StdEnv/2020 bwa/0.7.17 samtools/1.12
 # Go to directory with merged & trimmed fastqs
 cd /scratch/edegreef/whales/BOW_fastq/merged/trimmed
 
-# Map paired.fastq's to reference. 
-ls *_R1_paired.fastq.gz | sed 's/_R1_paired.fastq.gz$//' | parallel --jobs $SLURM_NTASKS 'bwa mem /scratch/edegreef/whales/ref_genomes/BOW_reference.fasta {}_R1_paired.fastq.gz {}_R2_paired.fastq.gz | samtools sort -T {}_tmp -o {}.sorted.bam'
+ref_genome=GCA_028564815.2_mEubGla1.1.hap2._XY_genomic.fna
+#narwhal: NAR_GCF_005190385.2.scafname.fasta
+#bowhead/rightwhale: GCA_028564815.2_mEubGla1.1.hap2._XY_genomic.fna
+
+# Map paired.fastq's to reference. May have to group some files into batches (in folders) to run more at once
+ls *_R1_paired.fastq.gz | sed 's/_R1_paired.fastq.gz$//' | parallel --jobs $SLURM_NTASKS 'bwa mem /scratch/edegreef/whales/ref_genomes/$ref_genome {}_R1_paired.fastq.gz {}_R2_paired.fastq.gz | samtools sort -T {}_tmp -o {}.sorted.bam'
 
 # Index bam file
 ls *.sorted.bam | parallel 'samtools index {}'
