eece932509d896fd92a60637168f5999c5a62d17
Evelien de Greef <49288304+edegreef@users.noreply.github.com>
2024-06-21 09:05:13 -0500

    Update 02-edit_sampleID.sh

--- snps_prep/02-edit_sampleID.sh
+++ snps_prep/02-edit_sampleID.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
-# Script for editing sample IDs in the vcf 
+# Script for editing labels in the vcf, shortening sample ID to remove unncessary characters/paths in sample IDs
+
 # Example: I want to shorten sample ID from "/edegreef/whales/BOW_bam/88_Pang_S51" to "88_Pang"
 # Can also go straight to "bcftools reheader" if want to use a pre-made sample ID list.
 
-snps=BOW_allvariantcalls_pp
+variants=bowhead_RWmap_allvariants
 
-# Zip and index vcf if not done already
-bgzip -c $snps.vcf > $snps.vcf.gz
-tabix -p vcf $snps.vcf.gz
+#1) Change sample ID in vcf to just contain the individual ID (removing the path that is currently in sample name).
+
+# Zip and make index file if not done already
+bgzip -c $variants.vcf > $variants.vcf.gz
+tabix -p vcf $variants.vcf.gz
 
 # Make list of current sample ID names
-vcf-query -l $snps.vcf.gz > vcf_sampleID
+vcf-query -l $variants.vcf.gz > vcf_sampleID
 
 # Shorten sample IDs by removing text up to the last "/"
 awk '{print $NF}' FS=/ vcf_sampleID > vcf_sampleID_shortened
 
-# I also want to remove last 4 characters in my sample ID (e.g. _S43 or something from sequencing lane), not related to sample ID
+# also want to remove last 4 characters (e.g. _S43 or something from sequencing lane), not related to sample ID
 sed 's/....$//' vcf_sampleID_shortened > vcf_sampleID_shortened2
 
 # Modify header in vcf file to change sample ID to the shortened ID list
-bcftools reheader -s vcf_sampleID_shortened2 $snps.vcf.gz -o $snps.ID.vcf.gz
+/home/degreefe/programs/bcftools-1.9/bcftools reheader -s vcf_sampleID2 $variants.vcf.gz -o $variants.ID.vcf.gz
+tabix -p vcf $variants.ID.vcf.gz
 
-# Index new vcf file
-tabix -p vcf $snps.ID.vcf.gz
