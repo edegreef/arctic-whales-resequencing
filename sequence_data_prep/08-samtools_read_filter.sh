e023fdadd5b440bcf368a64de2005296bdd51712
Evelien de Greef <49288304+edegreef@users.noreply.github.com>
2024-06-21 08:55:36 -0500

    Update 08-samtools_read_filter.sh

--- sequence_data_prep/08-samtools_read_filter.sh
+++ sequence_data_prep/08-samtools_read_filter.sh
@@ -3,19 +3,20 @@
 #SBATCH --time=48:00:00
 #SBATCH --account=def-coling
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=edegreef@ucdavis.edu
+#SBATCH --mail-user=evelien.degreef@umanitoba.ca
 #SBATCH --mem=20G
 #SBATCH --cpus-per-task=4
 #SBATCH --job-name=samtools_read_filter
 #SBATCH --output=%x-%j.out
 
 cd /scratch/edegreef/whales/BOW_bam/dedupRG
+mkdir samtools_filter
 
 module load nixpkgs/16.09 gcc/8.3.0 samtools/1.9
 
 # the -F 256 removes non-primary alignments. the -f 2 keeps only read mapped in proper pair
 
-# one way, but accidentally turns files into "...deDupRG.bam.pp.bam"
+# this works but does add an extra ".bam" in the file name. "...deDupRG.bam.pp.bam"
 for i in *deDupRG.bam
 do
 samtools view -b -h -F 256 -f 2 $i > samtools_filter/$i.pp.bam
