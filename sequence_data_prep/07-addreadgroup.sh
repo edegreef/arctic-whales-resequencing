300bf0e602a3e747233a403f3be99faa9e883b68
Evelien de Greef <49288304+edegreef@users.noreply.github.com>
2024-06-21 08:51:07 -0500

    Update 07-addreadgroup.sh

--- sequence_data_prep/07-addreadgroup.sh
+++ sequence_data_prep/07-addreadgroup.sh
@@ -3,7 +3,7 @@
 #SBATCH --time=24:00:00
 #SBATCH --account=def-coling
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=edegreef@ucdavis.edu
+#SBATCH --mail-user=evelien.degreef@umanitoba.ca
 #SBATCH --constraint=skylake
 #SBATCH --cpus-per-task=48
 #SBATCH --ntasks=1
@@ -16,7 +16,9 @@
 module load nixpkgs/16.09 gcc/8.3.0 picard/2.20.6 samtools/1.9
 
 # Create reference genome dictionary (.dict file)
-java -jar $EBROOTPICARD/picard.jar CreateSequenceDictionary R=/scratch/edegreef/ref_genomes/BOW_reference.fasta O=/scratch/edegreef/ref_genomes/BOW_reference.dict 
+# narwhal: NAR_GCF_005190385.2.scafname.fasta
+# bowhead/right whale: GCA_028564815.2_mEubGla1.1.hap2._XY_genomic.fna
+java -jar $EBROOTPICARD/picard.jar CreateSequenceDictionary R=/scratch/edegreef/ref_genomes/GCA_028564815.2_mEubGla1.1.hap2._XY_genomic.fna O=/scratch/edegreef/ref_genomes/GCA_028564815.2_mEubGla1.1.hap2._XY_genomic.dict 
 
 # Add read group information to each deDup bam
 ls /scratch/edegreef/whales/BOW_bam/*deDup.bam | sed 's/.deDup.bam$//' | parallel --jobs 8 'java -jar $EBROOTPICARD/picard.jar AddOrReplaceReadGroups I={}.deDup.bam O={}.deDupRG.bam RGID={} RGPL=illumina RGSM={} RGLB=lib1 RGPU=unit1'
