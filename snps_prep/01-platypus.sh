894f962a8f0b5492fcb1ada3c648332d9bb350d4
Evelien de Greef <49288304+edegreef@users.noreply.github.com>
2024-06-21 08:59:20 -0500

    Update 01-platypus.sh

--- snps_prep/01-platypus.sh
+++ snps_prep/01-platypus.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --time=12:00:00
+#SBATCH --time=24:00:00
 #SBATCH --account=def-coling
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=evelien.degreef@umanitoba.ca
@@ -13,16 +13,14 @@
 # Load modules
 module load nixpkgs/16.09 gcc/7.3.0 platypus/0.8.1
 
-# set up for bowhead whale
-# same set up for narwhal except for using "narwhal_bams.txt" and "NAR_reference.fasta" and output for narwhal name.
-
-# Go to directory with final bam files (in hindsight can just make path to each bam file in the .txt list file instead of going to folder).
-cd /scratch/edegreef/whales/dedupRG_bam/samtools_filter/downsampled
+# Go to directory with final bam files
+cd /scratch/edegreef/bowhead
 
 # Run platypus to call variants from bams & reference genome 
+# Here is for bowhead whale, just change the bam file list and reference genome, and output file name for narwhal files
 # Make sure all bam files are indexed beforehand. Also couldn't add Platypus.py to path for some reason so I inputed the path here in script
 python /cvmfs/soft.computecanada.ca/easybuild/software/2017/avx2/Compiler/gcc7.3/platypus/0.8.1/bin/Platypus.py callVariants \
---bamFiles=bowhead_whale_bams.txt \
---refFile=/scratch/edegreef/whales/ref_genomes/rclone/BOW_reference.fasta \
---output=/scratch/edegreef/whales/bowhead_allvariantcalls_pp.vcf \
+--bamFiles=bowhead_bams_list.txt \
+--refFile=/scratch/edegreef/bowhead/ref_genome/GCF_009873245.2_mBalMus1.pri.v3_genomic.fna \
+--output=/scratch/edegreef/bowhead/bowhead_RWmap_allvariants.vcf \
 --nCPU=32 --minReads=4
