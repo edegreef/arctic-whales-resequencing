aa866669e633c4ea354522dc54cca05d98f9ff5f
Evelien de Greef <49288304+edegreef@users.noreply.github.com>
2024-06-24 14:18:13 -0500

    Update 01-smcpp_prep.sh

--- demography/smcpp/01-smcpp_prep.sh
+++ demography/smcpp/01-smcpp_prep.sh
@@ -2,50 +2,28 @@
 
 ### preparing mask file for smcpp
 
-# Notes on installing SMC++ and preparing input files
-
-# download smc++
-wget https://github.com/popgenmethods/smcpp/releases/download/v1.15.2/smcpp-1.15.2-Linux-x86_64.sh
-
-chmod +x smcpp-1.15.2-Linux-x86_64.sh
-
-# install
-./smcpp-1.15.2-Linux-x86_64.sh
-
-# go through reading and acceptng license - answer 'yes' at the end, then program will be installed under /home/edegreef/smcpp
-
 # after installation finishes, then run smcpp/bin/activiate to use it
-source /home/edegreef/smcpp/bin/activate
+#source /home/degreefe/programs/smcpp/bin/activate
 
-# check it works
-smc++ {options}
-
-
-# prep input files
-# zip and index if not already done (for both RAW variants, and filtered variants)
-#bgzip BOW_SNPS.filter1.miss.biallel.min100kb.autosomes.vcf
-#tabix -p vcf BOW_SNPS.filter1.miss.biallel.min100kb.autosomes.vcf.gz
-
-cd /home/degreefe/whales/BOW_snps/smcpp
 
 # now make vcf of filtered out sites
-/home/degreefe/programs/bcftools-1.9/bcftools isec -p isec_filteredout BOW_allvariantcalls.ID.vcf.gz BOW_SNPS.filter1.miss.biallel.min100kb.autosomes.vcf.gz
+/home/degreefe/programs/bcftools-1.9/bcftools isec -p isec_filteredout bowhead_RWmap_allvariants.ID.vcf.gz bowhead_RWmap_snps_rmfix.filter1.miss.biallel.min100kb.autosomes.hwe.vcf.gz
 
-# isec_filteredout/0000.vcf is the one we want (snps that were filtered out)
+#isec_filteredout/0000.vcf is the one we want (snps that were filtered out)
 cp isec_filteredout/0000.vcf 0000.vcf
 
 # rename for smc input
 mv 0000.vcf smcpp_removed_sites.vcf
 
 # making bed file for masking
-export PATH=$PATH:/home/degreefe/programs/bedops/bin
+#export PATH=$PATH:/home/degreefe/programs/bedops/bin
 
 # install bedops
-cd /home/degreefe/programs
-mkdir bedops
-cd bedops
-wget https://github.com/bedops/bedops/releases/download/v2.4.39/bedops_linux_x86_64-v2.4.39.tar.bz2
-tar jxvf bedops_linux_x86_64-v2.4.39.tar.bz2
+#cd /home/degreefe/programs
+#mkdir bedops
+#cd bedops
+#wget https://github.com/bedops/bedops/releases/download/v2.4.39/bedops_linux_x86_64-v2.4.39.tar.bz2
+#tar jxvf bedops_linux_x86_64-v2.4.39.tar.bz2
 
 export PATH=$PATH:/home/degreefe/programs/bedops/bin
 
