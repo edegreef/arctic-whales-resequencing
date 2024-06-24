d958008821c6995904b97d13d7791809b985da60
Evelien de Greef <49288304+edegreef@users.noreply.github.com>
2024-06-24 14:07:35 -0500

    Update 04-psmc_plot.sh

--- demography/psmc/04-psmc_plot.sh
+++ demography/psmc/04-psmc_plot.sh
@@ -1,11 +1,17 @@
-# bowhead whale
-/home/degreefe/programs/psmc/utils/psmc_plot.pl -R -u 2.68e-08 -g 52.3 -p 88_Pang.N25.t15.r5_52.3_plot 88_Pang.N25.t15.r5.psmc
+# bowhead whale - main run
+/home/degreefe/programs/psmc/utils/psmc_plot.pl -R -u 2.68e-08 -g 52.3 -p 88_Pang_S51.rightwhale.N25.t15.r5_52.3_plot 88_Pang_S51.rightwhale.N25.t15.r5.psmc
+/home/degreefe/programs/psmc/utils/psmc_plot.pl -R -u 2.68e-08 -g 52.3 -p BM_RB_2005_001_S43.rightwhale.N25.t15.r5_52.3_plot BM_RB_2005_001_S43.rightwhale.N25.t15.r5.psmc
 
-# bowhead whale bootstrap
-/home/degreefe/programs/psmc/utils/psmc_plot.pl -R -u 2.68e-08 -g 52.3 -pY50000 combined_mugen combined88_Pang.psmc
+# bowhead whale - bootstrap
+/home/degreefe/programs/psmc/utils/psmc_plot.pl -R -u 2.69e-08 -g 52.3 -p 88_Pang_S51.rightwhale_2.69e-8_g21.9.BOOT.plot combined.88_Pang_S51.rightwhale.psmc
+/home/degreefe/programs/psmc/utils/psmc_plot.pl -R -u 2.69e-08 -g 52.3 -p BM_RB_2005_001_S43.rightwhale_2.69e-8_g21.9.BOOT.plot combined.BM_RB_2005_001_S43.rightwhale.psmc
 
-# narwhal
+# narwhal - main run
+/home/degreefe/programs/psmc/utils/psmc_plot.pl -R -u 1.56e-08 -g 21.9 -p ARCR_07_1065_1.56e-8_g21.9plot ARCR_07_1065.N25.t15.r5.psmc
+/home/degreefe/programs/psmc/utils/psmc_plot.pl -R -u 1.56e-08 -g 21.9 -p ARGF_07_1127_1.56e-8_g21.9plot ARGF_07_1127.N25.t15.r5.psmc
 /home/degreefe/programs/psmc/utils/psmc_plot.pl -R -u 1.56e-08 -g 21.9 -p ARRB_99_1026_1.56e-8_g21.9plot ARRB_99_1026.N25.t15.r5.psmc
 
-# narwhal bootstrap
-/home/degreefe/programs/psmc/utils/psmc_plot.pl -R -pY50000 combined combined.ARRB_99_1026.psmc
+# narwhal - bootstrap
+/home/degreefe/programs/psmc/utils/psmc_plot.pl -R -u 1.56e-08 -g 21.9 -p ARCR_07_1065_1.56e-8_g21.9.BOOT.plot combined.ARCR_07_1065.psmc
+/home/degreefe/programs/psmc/utils/psmc_plot.pl -R -u 1.56e-08 -g 21.9 -p ARGF_07_1127_1.56e-8_g21.9.BOOT.plot combined.ARGF_07_1127.psmc
+/home/degreefe/programs/psmc/utils/psmc_plot.pl -R -u 1.56e-08 -g 21.9 -p ARRB_99_1026_1.56e-8_g21.9.BOOT.plot combined.ARRB_99_1026.psmc
