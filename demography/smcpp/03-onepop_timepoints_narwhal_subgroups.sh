307406b49078897ab1f4a3750c17e1888dcdaf31
Evelien de Greef <49288304+edegreef@users.noreply.github.com>
2024-06-24 14:33:16 -0500

    Update 03-onepop_timepoints_narwhal_subgroups.sh

--- demography/smcpp/03-onepop_timepoints_narwhal_subgroups.sh
+++ demography/smcpp/03-onepop_timepoints_narwhal_subgroups.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
 #SBATCH --mail-user=evelien.degreef@umanitoba.ca
 #SBATCH --mail-type=ALL
-#SBATCH --time=24:00:00
+#SBATCH --time=12:00:00
 #SBATCH --mem=80000M
 #SBATCH --array=1-100
-#SBATCH --job-name=estimate_timepoints_nargr2
+#SBATCH --job-name=estimate_timepoints_narwhal_sub
+#SBATCH --account=def-coling
 
 # 100 array runs/iterations of 'smc++ estimate' for a population (this script is for one population)
-# also remember to make the folders first ("smc_out_timepoints", and a folder for each pop ID if running more later (here I used "1group"))
+# also remember to make the folders first ("smc_out_timepoints_subgroup", and a folder for each pop)
 
 source /home/edegreef/smcpp/bin/activate
 
-# Western Baffin Bay
-mkdir /scratch/edegreef/whales/NAR_smcpp/smc_out_timepoints/NOR_PURP/run_${SLURM_ARRAY_TASK_ID}/
-echo smc_out_timepoints/AB/run_${SLURM_ARRAY_TASK_ID}/
 # Run smc++ estimate
-smc++ estimate 1.56e-8 -o /scratch/edegreef/whales/NAR_smcpp/smc_out_timepoints/NOR_PURP/run_${SLURM_ARRAY_TASK_ID}/ --regularization-penalty 4.0 --nonseg-cutoff 100000 --thinning 2000 --cores 4 --timepoints 10 10000 /scratch/edegreef/whales/NAR_smcpp/masked_data_subgroup/NOR_PURP.*
 
-# Eastern Baffin Bay
-mkdir /scratch/edegreef/whales/NAR_smcpp/smc_out_timepoints/MID_BLU/run_${SLURM_ARRAY_TASK_ID}/
-echo smc_out_timepoints/AB/run_${SLURM_ARRAY_TASK_ID}/
+# Canadiah High Arctic / Western Baffin Bay
+mkdir /scratch/edegreef/narwhal_smcpp/smc_out_timepoints_subgroup/WBB/run_${SLURM_ARRAY_TASK_ID}/
+echo smc_out_timepoints_subgroup/WBB/run_${SLURM_ARRAY_TASK_ID}/
 # Run smc++ estimate
-smc++ estimate 1.56e-8 -o /scratch/edegreef/whales/NAR_smcpp/smc_out_timepoints/MID_BLU/run_${SLURM_ARRAY_TASK_ID}/ --regularization-penalty 4.0 --nonseg-cutoff 100000 --thinning 2000 --cores 4 --timepoints 10 10000 /scratch/edegreef/whales/NAR_smcpp/masked_data_subgroup/MID_BLU.*
+smc++ estimate 1.56e-8 -o /scratch/edegreef/narwhal_smcpp/smc_out_timepoints_subgroup/WBB/run_${SLURM_ARRAY_TASK_ID}/ --regularization-penalty 4.0 --nonseg-cutoff 100000 --thinning 2000 --cores 4 --timepoints 10 10000 /scratch/edegreef/narwhal_smcpp/masked_data_subgroup/WBB.*
+
+# Baffin Island / Eastern Baffin Bay
+mkdir /scratch/edegreef/narwhal_smcpp/smc_out_timepoints_subgroup/EBB/run_${SLURM_ARRAY_TASK_ID}/
+echo smc_out_timepoints_subgroup/EBB/run_${SLURM_ARRAY_TASK_ID}/
+# Run smc++ estimate
+smc++ estimate 1.56e-8 -o /scratch/edegreef/narwhal_smcpp/smc_out_timepoints_subgroup/EBB/run_${SLURM_ARRAY_TASK_ID}/ --regularization-penalty 4.0 --nonseg-cutoff 100000 --thinning 2000 --cores 4 --timepoints 10 10000 /scratch/edegreef/narwhal_smcpp/masked_data_subgroup/EBB.*
 
 # Repulse Bay / northern Hudson Bay
-mkdir /scratch/edegreef/whales/NAR_smcpp/smc_out_timepoints/RB/run_${SLURM_ARRAY_TASK_ID}/
-echo smc_out_timepoints/RB/run_${SLURM_ARRAY_TASK_ID}/
+mkdir /scratch/edegreef/narwhal_smcpp/smc_out_timepoints_subgroup/RB/run_${SLURM_ARRAY_TASK_ID}/
+echo smc_out_timepoints_subgroup/RB/run_${SLURM_ARRAY_TASK_ID}/
 # Run smc++ estimate
-smc++ estimate 1.56e-8 -o /scratch/edegreef/whales/NAR_smcpp/smc_out_timepoints/RB/run_${SLURM_ARRAY_TASK_ID}/ --regularization-penalty 4.0 --nonseg-cutoff 100000 --thinning 2000 --cores 4 --timepoints 10 10000 /scratch/edegreef/whales/NAR_smcpp/masked_data_subgroup/RB.*
+smc++ estimate 1.56e-8 -o /scratch/edegreef/narwhal_smcpp/smc_out_timepoints_subgroup/RB/run_${SLURM_ARRAY_TASK_ID}/ --regularization-penalty 4.0 --nonseg-cutoff 100000 --thinning 2000 --cores 4 --timepoints 10 10000 /scratch/edegreef/narwhal_smcpp/masked_data_subgroup/RB.*
