833b560bcbc88cc52b1eabc2658ab0241a6f8888
Evelien de Greef <49288304+edegreef@users.noreply.github.com>
2024-09-11 14:11:34 -0500

    Update 03-merge_fastqs.sh

--- sequence_data_prep/03-merge_fastqs.sh
+++ sequence_data_prep/03-merge_fastqs.sh
@@ -3,6 +3,9 @@
 # Merge raw fastq.gz files for reads across multiple lanes.
 # Code obtained from Paul at https://www.biostars.org/p/317385/
 
+# will add note that if doing on trimmed reads that have "paired" in the file name, might need to adjust a bit
+# for i in $(find ./ -type f -name "*.fastq.gz" | while read F; do basename $F | rev | cut -c 25- | rev; done | sort | uniq)
+
 for i in $(find ./ -type f -name "*.fastq.gz" | while read F; do basename $F | rev | cut -c 22- | rev; done | sort | uniq)
 
     do echo "Merging R1"
